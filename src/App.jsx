7f0f736de45c79315c80292c0f87aad18fc4e4a5
gaellebega <gaellebega@gmail.com>
2026-09-21 15:53:40 +0200

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-black">
+        ImageHolder App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
