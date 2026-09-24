912b25034cb845c307de578e19aea2de1d977dce
gaellebega <gaellebega@gmail.com>
2026-09-24 16:12:59 +0200

    images importation correction

--- src/data/artwork.js
+++ src/data/artwork.js
@@ -1,7 +1,7 @@
-import image1 from "./images/image1.jpg";
-import image2 from "./images/image2.jpg";
-import image3 from "./images/image3.jpg";
-import image4 from "./images/image4.jpg";
+import image1 from "../images/image1.jpg";
+import image2 from "../images/image2.jpg";
+import image3 from "../images/image3.jpg";
+import image4 from "../images/image4.jpg";
 
 const artwork = [
   {
